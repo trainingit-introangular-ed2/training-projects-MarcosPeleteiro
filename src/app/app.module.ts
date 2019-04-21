5170cd1bd6ebf81553fe6b053b0fe2316935ebd4
MarcosPeleteiro <49287472+MarcosPeleteiro@users.noreply.github.com>
2019-04-21 22:57:34 +0200

    'pub'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CoreModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,9 +5,10 @@ import { FooterComponent } from './layout/footer/footer.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { LayoutComponent } from './layout/layout.component';
 import { MainComponent } from './layout/main/main.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [HeaderComponent, MainComponent, FooterComponent, LayoutComponent],
+  declarations: [HeaderComponent, MainComponent, FooterComponent, LayoutComponent, NotFoundComponent],
   imports: [CommonModule, RouterModule],
   exports: [LayoutComponent]
 })
